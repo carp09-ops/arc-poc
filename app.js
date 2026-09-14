c19f76e9705f8d6fbf0ffc5cec81da9f80e5ee7a
carp09-ops <carp09@gmail.com>
2026-09-14 10:41:17 -0400

    feat: load training execution and history roundout

--- app.js
+++ app.js
@@ -3,9 +3,15 @@ roundoutStyle.rel = 'stylesheet';
 roundoutStyle.href = './core-roundout.css';
 document.head.appendChild(roundoutStyle);
 
+const trainingStyle = document.createElement('link');
+trainingStyle.rel = 'stylesheet';
+trainingStyle.href = './training-roundout.css';
+document.head.appendChild(trainingStyle);
+
 import './core-app.js';
 import './edge-workouts.js';
 import './qa-enhancements.js';
 import './starting-point.js';
 import './arc-roundout.js';
 import './body-analytics.js';
+import './training-roundout.js';
