30b6e86a6ed741061f6307b5022b9fae79019aae
carp09-ops <carp09@gmail.com>
2026-09-14 10:30:26 -0400

    feat: load Arc roundout and body analytics

--- app.js
+++ app.js
@@ -1,4 +1,11 @@
+const roundoutStyle = document.createElement('link');
+roundoutStyle.rel = 'stylesheet';
+roundoutStyle.href = './core-roundout.css';
+document.head.appendChild(roundoutStyle);
+
 import './core-app.js';
 import './edge-workouts.js';
 import './qa-enhancements.js';
 import './starting-point.js';
+import './arc-roundout.js';
+import './body-analytics.js';
